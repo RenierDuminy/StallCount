de70c266fdc044d76b64705aeb53aaa0ed311ad3
RenierDuminy <116361046+RenierDuminy@users.noreply.github.com>
2025-12-08 20:45:20 +0200

    Login fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.fkgekfd5u1g"
+    "revision": "0.adkpnmv2kvk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
