83378cd3715bb124623d553cf061a90a68bbadf3
RenierDuminy <116361046+RenierDuminy@users.noreply.github.com>
2026-01-09 18:18:22 +0200

    Vercel stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import AppRoutes from "./AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
 import ConnectionBanner from "./components/ConnectionBanner";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
   return (
     <AuthProvider>
       <ConnectionBanner />
       <AppRoutes />
+      <SpeedInsights />
     </AuthProvider>
   );
 }
